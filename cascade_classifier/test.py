ea593fff8069e5ae215d87a6ce1bf0bea0377a21
Marshal Wu <marshal.wu@gmail.com>
2013-07-17 15:18:19 +0800

    add cascade classifier test

---
+++ cascade_classifier/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+
+import cv2
+import datetime
+
+img = cv2.imread("p1.jpg")
+hc = cv2.CascadeClassifier("lbpcascade_frontalface.xml")
+
+now=datetime.datetime.now()
+faces = hc.detectMultiScale(img)
+print datetime.datetime.now()-now
+
+for face in faces:
+     cv2.rectangle(img, (face[0], face[1]), (face[0] + face[2], face[0] + face[3]), (255, 0, 0), 3)
+     
+cv2.imshow('face', img)
+
+if cv2.waitKey(15000) == 27:
+    cv2.destroyWindow('face')
+
+#cv2.imwrite("face.png", img)
